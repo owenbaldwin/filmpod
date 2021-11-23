c42c0448944cab5c7104529d7b491ccc950f276a
Emma Zurstrassen <emmazurs@gmail.com>
2021-11-23 16:05:49 +0000

    tasks controller index method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+  before_action :get_department
+
+  def index
+    @tasks = @department.tasks
+  end
+
+  private
+
+  def get_department
+    @department = Department.find(params[:department_id])
+  end
+
 end
