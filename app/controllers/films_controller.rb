4f7e3b072be270d8991ce8cf4650c7c8406d0ef3
Zoe Bell <zoecharlottebell@gmail.com>
2021-11-23 16:31:53 +0000

    most of films done

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,4 +1,8 @@
 class FilmsController < ApplicationController
+  def index
+    @films =Film.all
+  end
+
   def new
     @film = Film.new
   end
@@ -15,6 +19,25 @@ class FilmsController < ApplicationController
   def show
     @film = Film.find(params[:id])
   end
+
+  def edit
+    @film = Film.find(params[:id])
+  end
+
+  def update
+    @film = Film.find(params[:id])
+    @film.update(film_params)
+
+    redirect_to film_path(@film)
+  end
+
+  def destroy
+    @film = Film.find(params[:id])
+    @film.destroy
+
+    redirect_to films_path
+  end
+
   private
 
   def film_params
