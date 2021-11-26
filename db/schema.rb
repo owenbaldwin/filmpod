3393856ba8f204b16bf12b9da9398516a5e96c8c
Emma Zurstrassen <emmazurs@gmail.com>
2021-11-26 15:54:59 +0000

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_112133) do
+ActiveRecord::Schema.define(version: 2021_11_26_155328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2021_11_26_112133) do
     t.bigint "user_id", null: false
     t.string "title"
     t.text "content"
-    t.string "status"
+    t.string "status", default: "to-do"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position"
