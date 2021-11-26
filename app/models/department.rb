c991d011c6484e3a260d46e68a0c102e99e6b941
Owen Baldwin <owenabaldwin@gmail.com>
2021-11-26 13:18:33 +0000

    added images to department cards

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,5 @@
 class Department < ApplicationRecord
+  DPTNAMES = ["Direction", "Production", "Art Department", "Camera Department", "Acting Department", "Electric Department", "Grip Department", "Make-Up Department", "Wardrobe Department", "Sound Department", "Music Department", "Catering Department", "Stunts Department", "VFX Department", "Editing Department"].freeze
   belongs_to :film
   has_many :tasks, dependent: :destroy
   has_many :departments_users
