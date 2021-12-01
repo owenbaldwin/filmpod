a05477cedc49037ea746049dbe86460582805eda
Owen Baldwin <owenabaldwin@gmail.com>
2021-12-01 16:29:20 +0000

    dependent destroy done

--- app/models/departments_user.rb
+++ app/models/departments_user.rb
@@ -1,7 +1,7 @@
 class DepartmentsUser < ApplicationRecord
   belongs_to :department
   belongs_to :user
-  has_and_belongs_to_many :grants
-  has_and_belongs_to_many :films
+  has_and_belongs_to_many :grants, dependent: :destroy
+  has_and_belongs_to_many :films, dependent: :destroy
 
 end
