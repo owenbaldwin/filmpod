65f0f752cbaf669f0564f6d70bd6e12c69ba22a4
Emma Zurstrassen <emmazurs@gmail.com>
2021-12-01 12:17:05 +0000

    added due date to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_153420) do
+ActiveRecord::Schema.define(version: 2021_12_01_115147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2021_11_30_153420) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position"
+    t.date "due_date"
     t.index ["department_id"], name: "index_tasks_on_department_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
